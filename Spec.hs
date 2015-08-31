c94689de06b061262a0257857e1af741ec8230c6
Alvaro Garcia <derecerca@gmail.com>
2015-08-31 19:51:57 +0200

    red: a zero and a non-zero

--- Spec.hs
+++ Spec.hs
@@ -8,6 +8,9 @@ main = hspec $ do
       it "should sum two zeroes" $ do
         calculate "0+0" `shouldBe` (0 :: Int)
 
+      it "should sum a zero with a non-zero" $ do
+        calculate "0+1" `shouldBe` (1 :: Int)
+
 
 calculate :: String -> Int
 calculate _ = 0
