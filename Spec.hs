50ceaa2e604810fd16586d16620c0735ad8f2ef6
Alvaro Garcia <derecerca@gmail.com>
2015-08-31 19:45:55 +0200

    red: calculate the sum of two zeroes

---
+++ Spec.hs
@@ -0,0 +1,9 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+main :: IO ()
+main = hspec $ do
+    describe "String calculator" $ do
+      it "should sum two zeroes" $ do
+        calculate "0+0" `shouldBe` (0 :: Int)
