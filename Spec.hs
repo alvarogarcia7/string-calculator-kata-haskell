fd475834792aa2ca6f594d56b53f7822c828c0fe
Alvaro Garcia <derecerca@gmail.com>
2015-08-31 19:50:26 +0200

    green: use a scaffold for a hardcoded return

--- Spec.hs
+++ Spec.hs
@@ -7,3 +7,7 @@ main = hspec $ do
     describe "String calculator" $ do
       it "should sum two zeroes" $ do
         calculate "0+0" `shouldBe` (0 :: Int)
+
+
+calculate :: String -> Int
+calculate _ = 0
