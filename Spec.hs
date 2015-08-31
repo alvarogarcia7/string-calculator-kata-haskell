c0d312a9fc4f02d487f5c8cf8b4b227e8e9433fc
Alvaro Garcia <derecerca@gmail.com>
2015-08-31 20:15:55 +0200

    refactor: move the sums to another describe in preparation of the following operation

--- Spec.hs
+++ Spec.hs
@@ -6,16 +6,17 @@ import qualified Data.Text as T
 main :: IO ()
 main = hspec $ do
     describe "String calculator" $ do
-      it "should sum two zeroes" $ do
-        calculate "0+0" `shouldBe` (0 :: Int)
-
-      it "should sum a zero with a non-zero" $ do
-        calculate "0+1" `shouldBe` (1 :: Int)
-
-      it "should sum a non-zero with something" $ do
-        calculate "1+1" `shouldBe` (2 :: Int)
-
-
+      describe "should sum:" $ do
+        it "two zeroes" $ do
+          calculate "0+0" `shouldBe` (0 :: Int)
+  
+        it "a zero with a non-zero" $ do
+          calculate "0+1" `shouldBe` (1 :: Int)
+  
+        it "a non-zero with something" $ do
+          calculate "1+1" `shouldBe` (2 :: Int)
+  
+  
 calculate :: String -> Int
 calculate expression = first_operand + second_operand
     where parts' = T.chunksOf 1 $ T.pack expression
